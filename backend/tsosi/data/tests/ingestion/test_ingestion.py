d493d8257061ac376ca032f7d4b0864d0e8fcd2b
Guillaume Alzieu <guillaume.alzieu@univ-grenoble-alpes.fr>
2025-07-18 13:17:09 +0000

    Write tests for reworded data loads update

--- backend/tsosi/data/tests/ingestion/test_ingestion.py
+++ backend/tsosi/data/tests/ingestion/test_ingestion.py
@@ -4,7 +4,7 @@ import pytest
 import tsosi.data.preparation.raw_data_config as dc
 from django.core.exceptions import ObjectDoesNotExist
 from tsosi.data.ingestion.core import ingest
-from tsosi.models import DataLoadSource, Transfer
+from tsosi.models import DataLoadSource, Entity, Transfer
 from tsosi.tasks import ingest_test
 
 from ..factories import DataLoadSourceFactory, TransferFactory
@@ -19,18 +19,25 @@ def test_ingest_test_data(registries):
 @pytest.mark.django_db
 def test_old_data_load_deletion(datasources):
     print("Testing the deletion of old data loads.")
-    kwargs = {
+    kwargs_base = {
         "data_source_id": "pci",
         "data_load_name": "test_load",
         "date_data_obtained": datetime.date.today(),
         "full_data": True,
     }
-    dls = DataLoadSourceFactory.create(**kwargs)
-    t_1 = TransferFactory.create(data_load_source=dls)
-    t_2 = TransferFactory.create(data_load_source=dls)
-    t_3 = TransferFactory.create(data_load_source=dls)
 
-    test = dc.DataLoadSource(**kwargs)
+    kwargs_1 = {**kwargs_base, "year": 2020}
+    dls_1 = DataLoadSourceFactory.create(**kwargs_1)
+    t_1 = TransferFactory.create(data_load_source=dls_1)
+    t_2 = TransferFactory.create(data_load_source=dls_1)
+    t_3 = TransferFactory.create(data_load_source=dls_1)
+
+    kwargs_2 = {**kwargs_base, "year": 2021}
+    dls_2 = DataLoadSourceFactory.create(**kwargs_2)
+    t_4 = TransferFactory.create(data_load_source=dls_2)
+    t_5 = TransferFactory.create(data_load_source=dls_2)
+
+    test = dc.DataLoadSource(**kwargs_base)
     test_data = [
         {
             "emitter_name": "E_1",
@@ -55,7 +62,6 @@ def test_old_data_load_deletion(datasources):
             "raw_data": {},
         },
     ]
-
     data_ingestion_config = dc.DataIngestionConfig(
         date_generated=datetime.datetime.now(datetime.UTC).isoformat(
             timespec="seconds"
@@ -67,13 +73,23 @@ def test_old_data_load_deletion(datasources):
 
     ingest(data_ingestion_config, send_signals=False)
 
-    for t in [t_1, t_2, t_3]:
+    # Check deletion of old transfers
+    for t in [t_1, t_2, t_3, t_4, t_5]:
         with pytest.raises(ObjectDoesNotExist):
             Transfer.objects.get(pk=t.pk)
 
-    with pytest.raises(ObjectDoesNotExist):
-        DataLoadSource.objects.get(pk=dls.pk)
+        # Ensure entities are not deleted
+        emitter = Entity.objects.get(pk=t.emitter.pk)
+        recipient = Entity.objects.get(pk=t.recipient.pk)
+        if t.agent:
+            agent = Entity.objects.get(pk=t.agent.pk)
 
+    # Check deletion of old data loads
+    for dls in [dls_1, dls_2]:
+        with pytest.raises(ObjectDoesNotExist):
+            DataLoadSource.objects.get(pk=dls.pk)
+
+    # Check "correct" ingestion
     load_sources = DataLoadSource.objects.all()
     assert len(load_sources) == 1
 
