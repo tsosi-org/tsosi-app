e9779ddf2751eab3ac2c05ceca270d9aa50d1fcb
Baptiste Lefeuvre <baptiste.lefeuvre@univ-grenoble-alpes.fr>
2025-12-10 16:28:31 +0100

    Update supported_infra

--- backend/tsosi/models/static_data.py
+++ backend/tsosi/models/static_data.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from django.utils import timezone
@@ -66,150 +69,6 @@ def create_pid_registries():
         r.save()
 
 
-SUPPORTED_INFRASTRUCTURES = [
-    {
-        "id": "doaj",
-        "pid": {"registry_id": REGISTRY_ROR, "value": "05amyt365"},
-        "entity": {
-            "raw_name": "Directory of Open Access Journals",
-            "raw_website": "https://doaj.org",
-            "name": "Directory of Open Access Journals",
-            "website": "https://doaj.org",
-            "is_partner": True,
-            "short_name": "DOAJ",
-            "description": """DOAJ is a unique and extensive index of diverse open access journals from around the world, driven by a growing community, and is committed to insuring quality content is freely available online for everyone.""",
-        },
-        "infrastructure": {
-            "infra_finder_url": "https://infrafinder.investinopen.org/solutions/doaj-directory-of-open-access-journals",
-            "posi_url": "https://blog.doaj.org/2022/10/06/doaj-commits-to-the-principles-of-open-scholarly-infrastructure-posi",
-            "date_scoss_start": "2017-01-01",
-            "date_scoss_end": "2020-01-01",
-            "hide_amount": True,
-            "support_url": "https://doaj.org/support/",
-            "legal_entity_description": """
-                The legal entity is located in Denmark, see
-                <a href="https://www.wikidata.org/wiki/Q1227538" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    DOAJ on wikidata
-                </a>.
-            """,
-        },
-        "static_icon": "doaj_icon.png",
-    },
-    {
-        "id": "doab_oapen",
-        "pid": {"registry_id": REGISTRY_ROR, "value": "01q0bmy69"},
-        "entity": {
-            "raw_name": "OAPEN & DOAB",
-            "raw_website": "https://www.doabooks.org",
-            "raw_country": "NL",
-            "name": "OAPEN & DOAB",
-            "short_name": "OAPEN & DOAB",
-            "website": "https://www.doabooks.org",
-            "country": "NL",
-            "description": """OAPEN is an open infrastructure dedicated to promoting and supporting the transition to open access for books. The Directory of Open Access Books (DOAB) is an open, global, and trusted community-driven discovery service that indexes and provides free access to scholarly, peer-reviewed open access books.""",
-            "manual_logo": True,
-            "date_inception": "2010-01-01",  # Inception date of OAPEN - DOAB was launched in 2013
-            "is_partner": True,
-        },
-        "infrastructure": {
-            "infra_finder_url": "https://infrafinder.investinopen.org/solutions/directory-of-open-access-books",
-            "posi_url": "https://oapen.hypotheses.org/524",
-            "date_scoss_start": "2019-12-01",
-            "date_scoss_end": "2022-12-01",
-            "hide_amount": True,
-            "support_url": "https://www.doabooks.org/en/librarians/how-to-become-a-doab-supporter",
-            "legal_entity_description": """
-                DOAB & OAPEN have the same legal entity, located in the Netherlands.
-                See
-                <a href="https://www.wikidata.org/wiki/Q21750281" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    DOAB on wikidata
-                </a>,
-                <a href="https://www.wikidata.org/wiki/Q109123141" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    OAPEN on wikidata
-                </a>.
-            """,
-        },
-        "static_logo": "LOGO_oapen_doab.png",
-        "static_icon": "doab_icon.ico",
-    },
-    {
-        "id": "operas",
-        "pid": {"registry_id": REGISTRY_ROR, "value": "00rfexj26"},
-        "entity": {
-            "raw_name": "OPERAS",
-            "raw_website": "https://operas-eu.org",
-            "name": "OPERAS",
-            "short_name": "OPERAS",
-            "website": "https://operas-eu.org",
-            "description": """OPERAS is the Research Infrastructure supporting open scholarly communication in the social sciences and humanities (SSH) in the European Research Area. Its mission is to coordinate and federate resources in Europe to efficiently address the scholarly communication needs of European researchers in the field of SSH.""",
-            "is_partner": True,
-        },
-        "infrastructure": {
-            "posi_url": "https://operas-eu.org/principles-of-open-scholarly-infrastructure-posi",
-            "support_url": "https://operas-eu.org/about/want-to-know-more/want-to-join-operas/",
-            "legal_entity_description": """
-                The legal entity is located in Belgium, see
-                <a href="https://www.wikidata.org/wiki/Q54879577" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    OPERAS on wikidata
-                </a>.
-            """,
-        },
-        "static_icon": "operas_icon.png",
-    },
-    {
-        "id": "pci",
-        "pid": {"registry_id": REGISTRY_ROR, "value": "0315saa81"},
-        "entity": {
-            "raw_name": "Peer Community In",
-            "raw_website": "https://peercommunityin.org",
-            "short_name": "PCI",
-            "name": "Peer Community In",
-            "website": "https://peercommunityin.org",
-            "is_partner": True,
-            "description": """PCI is a non-profit organisation of researchers offering peer review, recommendation and publication of scientific articles in open access for free.""",
-        },
-        "infrastructure": {
-            "infra_finder_url": "https://infrafinder.investinopen.org/solutions/peer-community-in",
-            "posi_url": "https://peercommunityin.org/2024/04/11/posi/",
-            "support_url": "https://peercommunityin.org/endorse-and-financially-support-pci/",
-            "legal_entity_description": """
-                The legal entity is located in France, see
-                <a href="https://www.wikidata.org/wiki/Q97368331" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    PCI on wikidata
-                </a>.
-            """,
-        },
-        "static_icon": "pci_icon.jpg",
-    },
-    {
-        "id": "scipost",
-        "pid": {"registry_id": REGISTRY_WIKIDATA, "value": "Q52663237"},
-        "entity": {
-            "raw_name": "SciPost",
-            "raw_website": "https://scipost.org",
-            "name": "SciPost",
-            "short_name": "SciPost",
-            "website": "https://scipost.org",
-            "is_partner": True,
-            "description": """SciPost is a complete diamond open access publishing infrastructure serving professional scientists worldwide.""",
-        },
-        "infrastructure": {
-            "posi_url": "https://scipost.org/posi",
-            "date_scoss_start": "2024-12-01",
-            "date_scoss_end": "2027-12-01",
-            "support_url": "https://scipost.org/sponsors/",
-            "legal_entity_description": """
-                The legal entity is located in the Netherlands, see
-                <a href="https://www.wikidata.org/wiki/Q52663237" rel="noopener noreferrer" target="_blank" class="wikidata-inline-link">
-                    SciPost on wikidata
-                </a>.
-            """,
-        },
-        "static_icon": "scipost_icon.png",
-    },
-]
-
-
 @transaction.atomic
 def update_partners():
     """
@@ -219,7 +78,13 @@ def update_partners():
     with the new identifier.
     """
     now = timezone.now()
-    for infra in SUPPORTED_INFRASTRUCTURES:
+    file_path = (
+        Path(__file__).resolve().parent.parent
+        / "data/assets/supported_infrastructures.json"
+    )
+    with open(file_path, "r") as f:
+        supported_infrastructures = json.load(f)
+    for infra in supported_infrastructures:
         create = False
         static_logo: str | None = infra.get("static_logo")
         static_icon: str | None = infra.get("static_icon")
