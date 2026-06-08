1d21f78e306f78008d9d6082a06348eaf5bcdc28
Baptiste Lefeuvre <baptiste.lefeuvre@univ-grenoble-alpes.fr>
2026-06-08 16:55:42 +0200

    Add whoweare view

---
+++ frontend/src/components/atoms/MemberAtom.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+import ExternalLinkAtom from "./ExternalLinkAtom.vue"
+import ImageAtom from "./ImageAtom.vue"
+const props = defineProps<{
+  name: string
+  role?: string
+  orcid?: string
+}>()
+</script>
+
+<template>
+  <div class="card">
+    <div class="name">
+      <span>{{ props.name }}</span>
+      <ExternalLinkAtom v-if="props.orcid" :href="props.orcid">
+        <ImageAtom
+          :src="'/img/orcid_badge.svg'"
+          :width="'1em'"
+          :container-padding="'0'"
+          style="display: inline-block; margin-right: 4px"
+        />
+      </ExternalLinkAtom>
+    </div>
+    <p class="role">{{ props.role }}</p>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 270px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  border-radius: 8px;
+  box-shadow:
+    rgba(0, 0, 0, 0.2) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.2) 0px 1px 2px -1px;
+  padding: 20px 10px;
+  position: relative;
+}
+
+.name span {
+  font-weight: 600;
+  font-size: 1.25rem;
+  line-height: 1.75rem;
+}
+
+.role {
+  position: absolute;
+  bottom: 10px;
+  text-align: center;
+  font-style: italic;
+  font-size: 0.9em;
+}
+
+.external-link {
+  margin-left: 8px;
+}
+</style>
---
+++ frontend/src/views/WhoView.vue
@@ -0,0 +1,101 @@
+<script setup lang="ts">
+import Member from "@/components/atoms/MemberAtom.vue"
+import StaticSectionAtom from "@/components/atoms/StaticSectionAtom.vue"
+import StaticContentComponent from "@/views/StaticContentView.vue"
+</script>
+
+<template>
+  <StaticContentComponent title="Who we are ?">
+    <StaticSectionAtom id="steering-comittee" title="Steering Committee">
+      <div class="container">
+        <Member
+          name="Marin Dacos"
+          role="Ministry of Higher Education, Research and Space"
+          orcid="https://orcid.org/0000-0002-9361-5295"
+        >
+        </Member>
+        <Member
+          name="Suzanne Dumouchel"
+          role="CNRS"
+          orcid="https://orcid.org/0000-0002-3569-532X"
+        >
+        </Member>
+        <Member
+          name="Maxence Larrieu"
+          role="Université Grenoble Alpes"
+          orcid="https://orcid.org/0000-0002-1834-3007"
+        >
+        </Member>
+      </div>
+    </StaticSectionAtom>
+    <StaticSectionAtom id="advisory-group" title="Advisory Group">
+      <div class="container">
+        <Member
+          name="Niels Stern"
+          role="DOAB"
+          orcid="https://orcid.org/0000-0001-6466-9748"
+        >
+        </Member>
+        <Member
+          name="Joanna Ball"
+          role="DOAJ"
+          orcid="https://orcid.org/0000-0002-4033-9119"
+        >
+        </Member>
+        <Member
+          name="Vanessa Proudman"
+          role="SparcEurope"
+          orcid="https://orcid.org/0000-0001-5623-9051"
+        >
+        </Member>
+      </div>
+    </StaticSectionAtom>
+    <StaticSectionAtom id="user-group" title="User Group">
+      <div class="container">
+        <Member
+          name="Denis Bourguet"
+          role="PeerCommunityIn"
+          orcid="https://orcid.org/0000-0002-2109-5323"
+        >
+        </Member>
+        <Member name="Thomas Guillemaud" role="PeerCommunityIn"> </Member>
+        <Member
+          name="Jean-Sebastien Caux"
+          role="SciPost"
+          orcid="https://orcid.org/0000-0001-9322-5011"
+        >
+        </Member>
+        <Member
+          name="Marlène Delhaye"
+          role="Couperin"
+          orcid="https://orcid.org/0000-0001-6306-5774"
+        >
+        </Member>
+      </div>
+    </StaticSectionAtom>
+    <StaticSectionAtom id="operational-team" title="Operational team">
+      <div class="container">
+        <Member name="Baptiste Lefeuvre" role="GRICAD, CNRS, UGA"> </Member>
+        <Member
+          name="Maxence Larrieu"
+          role="Université Grenoble Alpes"
+          orcid="https://orcid.org/0000-0002-1834-3007"
+        >
+        </Member>
+      </div>
+    </StaticSectionAtom>
+  </StaticContentComponent>
+</template>
+
+<style scoped>
+.container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 16px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+}
+</style>
